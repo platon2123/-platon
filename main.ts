f449df4820a14fa542bb16837adeab3eb5e75614
platon2123 <144277703+platon2123@users.noreply.github.com>
2023-12-15 16:42:31 +0200

    Початкові файли для проекту MakeCode

---
+++ main.ts
@@ -0,0 +1,88 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # . . .
+    . . # . .
+    . . . # .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . # # # .
+    . # . # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . . . #
+    . # # # .
+    . # # # .
+    . # # # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . . . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # . # . #
+    . # . # .
+    # . # . #
+    . # . # .
+    # . # . #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
